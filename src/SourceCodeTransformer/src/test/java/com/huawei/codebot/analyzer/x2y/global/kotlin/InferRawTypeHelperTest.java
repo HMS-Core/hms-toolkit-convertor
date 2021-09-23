40b04b4018675addbd4d3ddd0ace01f812a5b177
cloud-welink <limitface@foxmail.com>
2021-09-23 16:02:25 +0800

    增加测试项

--- src/SourceCodeTransformer/src/test/java/com/huawei/codebot/analyzer/x2y/global/kotlin/InferRawTypeHelperTest.java
+++ src/SourceCodeTransformer/src/test/java/com/huawei/codebot/analyzer/x2y/global/kotlin/InferRawTypeHelperTest.java
@@ -16,12 +16,42 @@
 package com.huawei.codebot.analyzer.x2y.global.kotlin;
 
 import com.huawei.codebot.analyzer.x2y.global.KotlinBasicType;
+import org.junit.Assert;
 
 public class InferRawTypeHelperTest {
 
-
     @org.junit.Test
     public void getRawType() {
+        Assert.assertEquals(inferRawType("Array"), InferRawTypeHelper.getRawType("Array").getQualifiedName());
+        Assert.assertEquals(inferRawType("arrayOf"), InferRawTypeHelper.getRawType("arrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("arrayOfNulls"), InferRawTypeHelper.getRawType("arrayOfNulls").getQualifiedName());
+        Assert.assertEquals(inferRawType("emptyArray"), InferRawTypeHelper.getRawType("emptyArray").getQualifiedName());
+        Assert.assertEquals(inferRawType("booleanArrayOf"), InferRawTypeHelper.getRawType("booleanArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("byteArrayOf"), InferRawTypeHelper.getRawType("byteArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("charArrayOf"), InferRawTypeHelper.getRawType("charArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("doubleArrayOf"), InferRawTypeHelper.getRawType("doubleArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("floatArrayOf"), InferRawTypeHelper.getRawType("floatArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("intArrayOf"), InferRawTypeHelper.getRawType("intArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("longArrayOf"), InferRawTypeHelper.getRawType("longArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("shortArrayOf"), InferRawTypeHelper.getRawType("shortArrayOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("List"), InferRawTypeHelper.getRawType("List").getQualifiedName());
+        Assert.assertEquals(inferRawType("listOf"), InferRawTypeHelper.getRawType("listOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("listOfNotNull"), InferRawTypeHelper.getRawType("listOfNotNull").getQualifiedName());
+        Assert.assertEquals(inferRawType("emptyList"), InferRawTypeHelper.getRawType("emptyList").getQualifiedName());
+        Assert.assertEquals(inferRawType("arrayListOf"), InferRawTypeHelper.getRawType("arrayListOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("mutableListOf"), InferRawTypeHelper.getRawType("mutableListOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("Map"), InferRawTypeHelper.getRawType("Map").getQualifiedName());
+        Assert.assertEquals(inferRawType("mapOf"), InferRawTypeHelper.getRawType("mapOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("hashMapOf"), InferRawTypeHelper.getRawType("hashMapOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("linkedMapOf"), InferRawTypeHelper.getRawType("linkedMapOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("mutableMapOf"), InferRawTypeHelper.getRawType("mutableMapOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("sortedMapOf"), InferRawTypeHelper.getRawType("sortedMapOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("Set"), InferRawTypeHelper.getRawType("Set").getQualifiedName());
+        Assert.assertEquals(inferRawType("setOf"), InferRawTypeHelper.getRawType("setOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("hashSetOf"), InferRawTypeHelper.getRawType("hashSetOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("linkedSetOf"), InferRawTypeHelper.getRawType("linkedSetOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("mutableSetOf"), InferRawTypeHelper.getRawType("mutableSetOf").getQualifiedName());
+        Assert.assertEquals(inferRawType("sortedSetOf"), InferRawTypeHelper.getRawType("sortedSetOf").getQualifiedName());
 
 
     }
